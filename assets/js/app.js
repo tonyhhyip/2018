dea11580a97bb5ebe32aa17b3f86211f3cd6cf40
Tony Yip <tony@opensource.hk>
2017-10-26 11:04:53 +0800

    Add scrollspy

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,14 @@
 import 'babel-polyfill';
 import './ga';
 import '../scss/app.scss';
+import './toc';
 
-/* globals $ */
+/* globals $, window */
+
+if (process.env.NODE_ENV === 'production' && window.location.host !== 'localhost' && window.location.protocol !== 'https:') {
+  window.location.href = window.location.href.replace('http://', 'https://');
+}
+
 $('.button-collapse').sideNav();
+$('.scrollspy').scrollSpy();
+
---
+++ assets/js/toc.js
@@ -0,0 +1,11 @@
+/* global $ */
+// Floating-Fixed table of contents
+setTimeout(() => {
+  const $nav = $('nav');
+  const $wrapper = $('.toc-wrapper');
+
+  $wrapper.pushpin({
+    top: $nav.height() + $('.theme-area').height() + 96,
+    offset: $nav.height(),
+  });
+}, 100);
