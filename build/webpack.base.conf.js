5a937ade8dcd964821bf7475322ea905bbb06678
Tony Yip <tony@opensource.hk>
2018-03-27 18:29:57 +0800

    Change in eslint

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -22,7 +22,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.jsx?$/,
+        test: /\.js|vue$/,
         loader: 'eslint-loader',
         enforce: 'pre',
         options: {
