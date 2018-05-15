33e5bda295fbc11067a34aef5a4f7deb578041eb
Koala Yeung <koalay@gmail.com>
2018-05-15 18:47:04 +0800

    fix https redirection detections
    * fix localhost detection in redirection script.
      prevent localhost build by redirected.

--- assets/js/https.js
+++ assets/js/https.js
@@ -1,4 +1,4 @@
 /* globals window */
-if (process.env.NODE_ENV === 'production' && window.location.host !== 'localhost' && window.location.protocol !== 'https:') {
+if (process.env.NODE_ENV === 'production' && window.location.hostname !== 'localhost' && window.location.protocol !== 'https:') {
   window.location.href = window.location.href.replace('http://', 'https://');
 }
